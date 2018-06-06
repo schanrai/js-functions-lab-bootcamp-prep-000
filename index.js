6dce3ce21b44ae2d558e5bef4ee06de1bde4193d
Sushi Chanrai <sushi@pathworks.co>
2018-06-06 19:21:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // write your code below!
+
 function happyHolidays(){
-return 'Happy holidays!';
+  return "Happy holidays!";
 }
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}`;
 }
-happyHolidaysTo('you');
+
 function happyHolidayTo(holiday,name){
-  return `Happy ${holiday}, ${name}!`; 
+  return `Happy ${holiday},my dear ${name}!`;
 }
-function holidayCountdown(holiday,days){
-  return `It's ${days} days until ${holiday}!`; 
-}
-holidayCountdown('Mother\s Day',20)
+
+happyHolidays();
+happyHolidaysTo(3);
+happyHolidayTo("passover","potatohead");
